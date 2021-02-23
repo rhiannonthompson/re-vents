093583b8bf14681a38cac72ec66b4af601860002
Rhiannon Thompson <tommywoops@gmail.com>
2021-02-23 22:23:20 +0000

    End of section 4

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -6,11 +6,28 @@ import Navbar from "../../features/nav/Navbar";
 
 function App() {
   const [formOpen, setFormOpen] = useState(false);
+  const [selectedEvent, setSelectedEvent] = useState(null);
+
+  function handleSelectEvent(event) {
+    setSelectedEvent(event);
+    setFormOpen(true);
+  }
+
+  function handleCreateFormOpen() {
+    setSelectedEvent(null);
+    setFormOpen(true);
+  }
+
   return (
     <>
-      <Navbar setFormOpen={setFormOpen} />
+      <Navbar setFormOpen={handleCreateFormOpen} />
       <Container className="main">
-        <EventDashboard setFormOpen={setFormOpen} formOpen={formOpen}/>
+        <EventDashboard
+          setFormOpen={setFormOpen}
+          formOpen={formOpen}
+          selectEvent={handleSelectEvent}
+          selectedEvent={selectedEvent}
+        />
       </Container>
     </>
   );
--- src/features/events/eventsDashboard/EventListItem.jsx
+++ src/features/events/eventsDashboard/EventListItem.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Icon, Item, List, Segment } from "semantic-ui-react";
 import EventListAttendee from "./eventListAttendee";
 
-export default function EventListItem({event}) {
+export default function EventListItem({event, selectEvent, deleteEvent}) {
   return (
     <Segment.Group>
       <Segment>
@@ -33,7 +33,18 @@ export default function EventListItem({event}) {
       </Segment>
       <Segment clearing>
         <div>{event.description}</div>
-        <Button color="teal" floated="right" content="View" />
+        <Button
+          color="red"
+          floated="right"
+          content="Delete"
+          onClick={() => deleteEvent(event.id)}
+        />
+        <Button
+          color="teal"
+          floated="right"
+          content="View"
+          onClick={() => selectEvent(event)}
+        />
       </Segment>
 
   </Segment.Group>
