69abbb006944896208f351ed051bddc1c0500a0b
Rhiannon Thompson <tommywoops@gmail.com>
2021-03-29 12:24:31 +0100

    End of section 15

--- src/app/config/firebase.js
+++ src/app/config/firebase.js
@@ -7,11 +7,12 @@ import "firebase/storage";
 const firebaseConfig = {
   apiKey: "AIzaSyB6H5487R9Q1YNSbYbW0p1gireDTY-OxNg",
   authDomain: "revents-1d3c6.firebaseapp.com",
+  databaseURL: "https://revents-1d3c6-default-rtdb.europe-west1.firebasedatabase.app",
   projectId: "revents-1d3c6",
   storageBucket: "revents-1d3c6.appspot.com",
   messagingSenderId: "1044517116399",
   appId: "1:1044517116399:web:117cb10a5e0e662721be1a"
-}
+};
 
 
 firebase.initializeApp(firebaseConfig);
--- src/features/events/eventActions.js
+++ src/features/events/eventActions.js
@@ -1,10 +1,20 @@
-import { CREATE_EVENT, DELETE_EVENT, FETCH_EVENTS, UPDATE_EVENT } from "./eventConstants";
-import { asyncActionError, asyncActionFinish, asyncActionStart } from "../../app/async/asyncReducer";
+import {
+  CREATE_EVENT,
+  DELETE_EVENT,
+  FETCH_EVENTS,
+  LISTEN_TO_EVENT_CHAT,
+  UPDATE_EVENT,
+} from "./eventConstants";
+import {
+  asyncActionError,
+  asyncActionFinish,
+  asyncActionStart,
+} from "../../app/async/asyncReducer";
 import { fetchSampleData } from "../../app/api/mockApi";
 
 export function loadEvents() {
   return async function (dispatch) {
-    dispatch(asyncActionStart())
+    dispatch(asyncActionStart());
     try {
       const events = await fetchSampleData();
       dispatch({ type: FETCH_EVENTS, payload: events });
@@ -12,35 +22,40 @@ export function loadEvents() {
     } catch (error) {
       dispatch(asyncActionError(error));
     }
-  }
+  };
 }
 
 export function listenToEvents(events) {
   return {
     type: FETCH_EVENTS,
     payload: events,
-  }
+  };
 }
-
-
 
 export function createEvent(event) {
   return {
     type: CREATE_EVENT,
-    payload: event
-  }
+    payload: event,
+  };
 }
 
 export function updateEvent(event) {
   return {
     type: UPDATE_EVENT,
-    payload: event
-  }
+    payload: event,
+  };
 }
 
 export function deleteEvent(eventId) {
   return {
     type: DELETE_EVENT,
-    payload: eventId
-  }
+    payload: eventId,
+  };
+}
+
+export function listenToEventChat(comments) {
+  return {
+    type: LISTEN_TO_EVENT_CHAT,
+    payload: comments,
+  };
 }
